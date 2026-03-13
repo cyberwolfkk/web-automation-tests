a0dfb49f941989572785bbdb91de085db47c548a
cyberwolfkk <cyberwolfkk@gmail.com>
2026-03-13 13:11:30 +0000

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,9 +5,14 @@ test('my flow', async ({ page }) => {
   // 1. Open page
   await page.goto('https://leofame.com/free-instagram-views');
 
-  // 2. Enter URL into input box
-  await page.fill('input[name="free_link"]', 'https://www.instagram.com/reel/DVyr1NgDFiZ/?igsh=MTlhc2Z2MjhjMjN0ZA==');
+  const input = page.locator('input[name="free_link"]');
 
+if (await input.count() > 0) {
+  console.log("Input field found");
+  await input.fill('https://www.instagram.com/reel/DVyr1NgDFiZ/');
+} else {
+  console.log("Input field not found");
+}
   // 3. Click button
   await page.click('button:has-text("Get free Views")');
 
